489c8c4d8b00c2a71b9b4658e12f6a8d36a41753
Aashish Maurya <aashixhmaurya@gmail.com>
2026-06-28 22:54:55 +0530

    Fix CORS configuration

--- server.js
+++ server.js
@@ -6,23 +6,7 @@ const { GoogleGenAI } = require('@google/genai');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configure CORS for local development
-const allowedOrigins = [
-  'http://127.0.0.1:5500',
-  'http://localhost:5500',
-  'http://127.0.0.1:3000',
-  'http://localhost:3000'
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(__dirname));
